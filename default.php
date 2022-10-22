29359ba0b113a97824bbcbdb7d1d9c4740c925c4
Chomchaba Chunakiat <Chomchaba.Chanakiat@gmail.com>
2022-10-22 21:18:52 +0700

    fix some bugs...

--- default.php
+++
@@ -1,1 +0,0 @@
-{"seatStatus":"red"}
--- script.js
+++ script.js
@@ -1,31 +1,78 @@
-const seats = document.querySelectorAll('.seat');
+const allSeats = document.querySelectorAll('.seat ,.book-seat');
+const statusSeats = document.querySelectorAll('.seat');
+const bookSeats = document.querySelectorAll('.book-seat');
+var showCaption = false;
 
-seats.forEach(seat => {
-  seat.addEventListener('click', function handleClick(event) {
-    var popup = seat.childNodes[1];
-    // console.log(popup.classList);
-    popup.classList.toggle("show");
-    // console.log(popup.parentNode.style.background);
-  });
-});
-
+var occupiedSeat;
 function startLiveUpdate() {
   setInterval(function() {
-    fetch('default.php').then(function(response) {
+    fetch('default.json').then(function(response) {
       return response.json();
     }).then(function(data) {
-      seats.forEach(seat => {
-        seat.style.background = data.seatStatus;
+      allSeats.forEach(seat => {
+        seat.style.background = "green";
       });
+      occupiedSeat = data.seatStatus.occupied
+      occupiedSeat.forEach(cell =>{
+        var seats = document.querySelectorAll('#'+cell.seatId);
+        seats.forEach(s => {
+          s.style.background = "red";
+          if (s.classList.contains('seat')) {
+            s.childNodes[1].textContent = cell.caption;
+          }
+        });
+      });
+
     }).catch(function (error) {
       console.log(error);
     })
-  },500); 
+
+    statusSeats.forEach(function(seat) {
+      var popup = seat.childNodes[1];
+      if (!showCaption) return;
+      if (popup.textContent == "") {
+        popup.classList.remove('show');
+      }
+      else {
+        popup.classList.add('show');
+      }
+    });
+
+  },2000); 
 }
 document.addEventListener('DOMContentLoaded', function() {
   startLiveUpdate();
 });
 
+statusSeats.forEach(seat => {
+  seat.addEventListener('click', function handleClick(event) {
+    // console.log(seat.childNodes);
+    var popup = seat.childNodes[1];
+    // console.log(popup.classList);
+    if (popup.textContent!='') popup.classList.toggle("show");
+    // console.log(popup.parentNode.style.background);
+  });
+});
+
+document.getElementById("caption-btn").addEventListener('click',toggleCaption);
+function toggleCaption() {
+  var btn = document.getElementById("caption-btn");
+  if (!showCaption) {
+    statusSeats.forEach(seat => {
+      if (seat.childNodes[1].textContent !='') {
+        seat.childNodes[1].classList.add('show');
+      }
+    });
+    btn.innerHTML = 'no caption';
+    showCaption = true;
+  } else {
+    statusSeats.forEach(seat => {
+      seat.childNodes[1].classList.remove('show');
+    });
+    btn.innerHTML = 'caption';
+    showCaption = false;
+  }
+};
 
 document.getElementById("floor-btn").addEventListener('click',toggleFloor);
 function toggleFloor() {
@@ -35,23 +82,6 @@ function toggleFloor() {
   } else {
     btn.innerHTML = 'first floor';
   }
-};
-
-document.getElementById("caption-btn").addEventListener('click',toggleCaption);
-function toggleCaption() {
-  var btn = document.getElementById("caption-btn");
-  if (btn.innerHTML == 'caption') {
-    seats.forEach(seat => {
-      seat.childNodes[1].classList.add('show');
-    });
-    btn.innerHTML = 'no caption';
-  } else {
-    seats.forEach(seat => {
-      seat.childNodes[1].classList.remove('show');
-    });
-    btn.innerHTML = 'caption';
-  }
-
 };
 
 
