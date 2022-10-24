3f4bbb4a4bbf76dd1a62f057c9bcf2b14b9aa28b
MiGene <92096007+MiGene@users.noreply.github.com>
2022-10-24 22:46:04 +0700

    extending2

---
+++ extendingScript.js
@@ -0,0 +1,14 @@
+var counter=10;
+    setInterval( function(){
+        counter--;
+
+        if(counter>=0) {
+            id = document.getElementById("countdown");
+            id.innerHTML = counter;
+        }
+
+        if(counter ===0){
+            id.innerHTML = "Time Out!!";
+        }
+    },1000);
+   
