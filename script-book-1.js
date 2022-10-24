620ee2f38515a5c9c8363ec0f9c66d2380a7b5b8
Chomchaba Chunakiat <Chomchaba.Chanakiat@gmail.com>
2022-10-24 15:31:52 +0700

    make map in book-2 selectable

--- script-book-1.js
+++ script-book-1.js
@@ -1,4 +1,12 @@
-function updateFriendList() {
+// -------------- function to be done first ----------------- //
+
+  // ------------------- all variable ----------------// 
+  var selectedFriendArray;
+  var indexOfCurrentSelectedFriend
+    
+  // ------- funciton for booking step 1 : choose your friend --------- //
+  
+  function updateFriendList() {
     fetch('account.json').then(function(response) {
         return response.json();
       }).then(function(data) {
@@ -7,25 +15,21 @@ function updateFriendList() {
         select.innerHTML += `<option value="" selected disabled hidden>-- select course --</option>`;
         data.friend.forEach(element => {
             var option = document.createElement("option");
-             option.text = element.studentId;
-             option.value = element.studentId;
+             option.text = element.name;
+             option.value = element.name + " " + element.studentId;
              select.add(option);
         }); 
     });
-}
-updateFriendList();
-
-var selectedFriend = new Set();
-document.getElementById('select-friend-btn').addEventListener('click',selectFriend);
-function selectFriend() {
+  }
+  function selectFriend() {
     let friend = document.getElementById('friend-list-selector').value;
-    selectedFriend.add(friend);
+    selectedFriendSet.add(friend);
     createSelectedFriendTable();
-}
-function createSelectedFriendTable() {
+  }
+  function createSelectedFriendTable() {
     let table = document.getElementById('friend-to-book-table');
     table.innerHTML = '';
-    for (const friend of selectedFriend) {
+    for (const friend of selectedFriendSet) {
         table.innerHTML += `
         <tr id='${"selected-"+friend}'>
             <td>${friend}</td>
@@ -37,13 +41,36 @@ function createSelectedFriendTable() {
     btn.forEach( b=> {
         let friend = b.id.slice(9)
         b.addEventListener('click', function handleClick(event) {
-                deleteSelectedFriend(b.id);
-                selectedFriend.delete(friend);
-          });
+          deleteSelectedFriend(b.id);
+          selectedFriendSet.delete(friend);
+        });
     });
-}
-
-function deleteSelectedFriend(id) {
+  }
+  function deleteSelectedFriend(id) {
     var element = document.getElementById(id);
     element.parentNode.removeChild(element);
-}
+  }
+  
+  function addContinueLoadFriendToNextHTML() {
+    const form = document.getElementById('form-select-seat');
+    form.addEventListener('submit',function(e) {
+      if (selectedFriendSet.size==0) return;
+      e.preventDefault();
+      const json = JSON.stringify(Array.from(selectedFriendSet));
+      localStorage.setItem('selected-friend-set',json);
+      window.location.href = "book-2.html";
+    })
+  }
+  
+  // ------------ coding begin here -------------------- //
+  // booking step 1 - select friend (id : select-friend)
+    var selectedFriendSet = new Set();
+    updateFriendList();
+    document.getElementById('select-friend-btn').addEventListener('click',selectFriend);
+  
+    addContinueLoadFriendToNextHTML(); 
+  
+  
+  //-------------------------------------//
+  
+  
--- script-home.js
+++ script-home.js
@@ -1,118 +1,142 @@
+// -------------- function to be done first ----------------- //
 function generateZoneCMap() {
-  var table = document.getElementById('zone-C-map');
-  for (let r = 1; r <= 8; r++) {
-    var idrow = "r" + r;
-    table.innerHTML += `<tr id='${idrow}'></tr>`
-    var item  = document.getElementById(idrow);
-    for (let c=1; c<=8; c++) {
-      let idcol = "c" + c;
-      let idseat = "zone-C-"+idrow + "-"+ idcol;
-      item.innerHTML += `<td id='${idcol}'>
-                          <div class="seat" id='${idseat}'>
-                            <span class="popuptext"></span>
-                          </div>
-                        </td>`
+    var seatType = "seat";
+    var elems = document.getElementById('book-map');
+    if (elems!=null) {
+      seatType = "book-seat";
+    }
+    var table = document.getElementById('zone-C-map');
+    for (let r = 1; r <= 8; r++) {
+      var idrow = "r" + r;
+      table.innerHTML += `<tr id='${idrow}'></tr>`
+      var item = document.getElementById(idrow);
+      for (let c=1; c<=8; c++) {
+        let idcol = "c" + c;
+        let idseat = "zone-C-"+idrow + "-"+ idcol;
+        item.innerHTML += `<td id='${idcol}'>
+                            <div class='${seatType}' id='${idseat}'>
+                              <span class="popuptext"></span>
+                            </div>
+                          </td>`
+      }
     }
   }
-}
-generateZoneCMap();
-
-const allSeats = document.querySelectorAll('.seat ,.book-seat');
-const statusSeats = document.querySelectorAll('.seat');
-const bookSeats = document.querySelectorAll('.book-seat');
-var showCaption = false;
-
-
-var occupiedSeat;
-function startLiveUpdate() {
-  setInterval(function() {
-    fetch('default.json').then(function(response) {
-      return response.json();
-    }).then(function(data) {
-      allSeats.forEach(seat => {
-        seat.style.background = "green";
-      });
-      occupiedSeat = data.seatStatus.occupied
-      occupiedSeat.forEach(cell =>{
-        var seats = document.querySelectorAll('#'+cell.seatId);
-        seats.forEach(s => {
-          s.style.background = "red";
-          if (s.classList.contains('seat')) {
-            s.childNodes[1].textContent = cell.caption;
-          }
+  // ------------------- all variable ----------------//
+  var statusSeats;
+  var showCaption;
+  var occupiedSeat;
+  var myInterval;
+    
+  // ---------------- function for home page --------------//
+  function seatStatusUpdate() {
+      fetch('default.json').then(function(response) {
+        return response.json();
+      }).then(function(data) {
+        statusSeats.forEach(seat => {
+          seat.style.background = "green";
         });
+        occupiedSeat = data.seatStatus.occupied
+        occupiedSeat.forEach(cell =>{
+          var seats = document.querySelectorAll('#'+cell.seatId);
+          seats.forEach(s => {
+            s.style.background = "red";
+            if (s.classList.contains('seat')) {
+              s.childNodes[1].textContent = cell.caption;
+            }
+          });
+        });
+  
+      }).catch(function (error) {
+        console.log(error);
+      })
+  
+      statusSeats.forEach(function(seat) {
+        var popup = seat.childNodes[1];
+        if (!showCaption) return;
+        if (popup.textContent == "") {
+          popup.classList.remove('show');
+        }
+        else {
+          popup.classList.add('show');
+        }
       });
-
-    }).catch(function (error) {
-      console.log(error);
-    })
-
-    statusSeats.forEach(function(seat) {
-      var popup = seat.childNodes[1];
-      if (!showCaption) return;
-      if (popup.textContent == "") {
-        popup.classList.remove('show');
-      }
-      else {
-        popup.classList.add('show');
-      }
-    });
-
-  },2000); 
-}
-document.addEventListener('DOMContentLoaded', function() {
-  startLiveUpdate();
-});
-
-statusSeats.forEach(seat => {
-  seat.addEventListener('click', function handleClick(event) {
-    // console.log(seat.childNodes);
-    var popup = seat.childNodes[1];
-    // console.log(popup.classList);
-    if (popup.textContent!='') popup.classList.toggle("show");
-    // console.log(popup.parentNode.style.background);
-  });
-});
-
-document.getElementById("caption-btn").addEventListener('click',toggleCaption);
-function toggleCaption() {
-  var btn = document.getElementById("caption-btn");
-  if (!showCaption) {
-    statusSeats.forEach(seat => {
-      if (seat.childNodes[1].textContent !='') {
-        seat.childNodes[1].classList.add('show');
-      }
-    });
-    btn.innerHTML = 'no caption';
-    showCaption = true;
-  } else {
-    statusSeats.forEach(seat => {
-      seat.childNodes[1].classList.remove('show');
-    });
-    btn.innerHTML = 'caption';
-    showCaption = false;
+   
   }
-};
+  
+  function addClickToShowCaption() {
+    statusSeats.forEach(seat => {
+      seat.addEventListener('click', function handleClick(event) {
+        var popup = seat.childNodes[1];
+        if (popup.textContent!='') popup.classList.toggle("show");
+        // console.log(popup.parentNode.style.background);
+      });
+    });
+  }
+  
+  function toggleCaption() {
+    var btn = document.getElementById("caption-btn");
+    if (!showCaption) {
+      statusSeats.forEach(seat => {
+        if (seat.childNodes[1].textContent !='') {
+          seat.childNodes[1].classList.add('show');
+        }
+      });
+      btn.innerHTML = 'no caption';
+      showCaption = true;
+    } else {
+      statusSeats.forEach(seat => {
+        seat.childNodes[1].classList.remove('show');
+      });
+      btn.innerHTML = 'caption';
+      showCaption = false;
+    }
+  };
+  
+  // ------------ coding begin here -------------------- //
 
+    // function to be done first
+    generateZoneCMap();
+  
+    // set default value
+    statusSeats = document.querySelectorAll('.seat');
+    showCaption = false;
+  
+    // home page (id : contain-map, status-map)
+       // real time status and caption
+        document.addEventListener('DOMContentLoaded', function() {
+          myInterval = setInterval(seatStatusUpdate, 2000);
+        });
+      // click event on seats;
+      addClickToShowCaption();
+      // add click event on caption toggle btn
+      if (document.getElementById("caption-btn")!=null) {
+        document.getElementById("caption-btn").addEventListener('click',toggleCaption);
+      } 
 
-// document.getElementById("floor-btn").addEventListener('click',toggleFloor);
-// function toggleFloor() {
-//   var btn = document.getElementById("floor-btn");
-//   if (btn.innerHTML == 'first floor') {
-//     btn.innerHTML = 'second floor';
-//   } else {
-//     btn.innerHTML = 'first floor';
-//   }
-// };
-
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     var els = document.querySelectorAll('.seat');
-//     els.forEach(function(cell) {
-//       if (cell.textContent == "Occupied") {
-//         cell.style.background ='red';
-//       }
-//       if (cell.textContent == "Vacant") {
-//         cell.style.background = 'green';
-//       }
-//     })
-//   })
+ 
+  
+  
+  //-------------------------------------//
+  
+  
+  // document.getElementById("floor-btn").addEventListener('click',toggleFloor);
+  // function toggleFloor() {
+  //   var btn = document.getElementById("floor-btn");
+  //   if (btn.innerHTML == 'first floor') {
+  //     btn.innerHTML = 'second floor';
+  //   } else {
+  //     btn.innerHTML = 'first floor';
+  //   }
+  // };
+  
+  // window.addEventListener('DOMContentLoaded', (event) => {
+  //     var els = document.querySelectorAll('.seat');
+  //     els.forEach(function(cell) {
+  //       if (cell.textContent == "Occupied") {
+  //         cell.style.background ='red';
+  //       }
+  //       if (cell.textContent == "Vacant") {
+  //         cell.style.background = 'green';
+  //       }
+  //     })
+  //   })
