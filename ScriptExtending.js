9655d65eeadbc0f262951dfc72b5cf8faa98fc72
MiGene <92096007+MiGene@users.noreply.github.com>
2022-10-26 18:01:49 +0700

    decorate

--- ScriptExtending.js
+++ ScriptExtending.js
@@ -1,4 +1,24 @@
-var counter=10;
+let countTime = new Date("Oct 26, 2022 20:00:00").getTime();
+
+let timer = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countTime - now;
+    var days = Math.floor(distance/ (1000*60*60*24));
+    var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    var minutes = Math.floor((distance% (1000*60*60)) / (1000*60));
+    var seconds = Math.floor((distance% (1000*60)) / 1000);
+
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (minutes==0 && seconds==0) {
+        console.log("helo");
+        window.location.href = "index-home.html";
+    }
+},1000)
+
+
+/*var counter=10;
     setInterval( function(){
         counter--;
 
@@ -11,5 +31,5 @@ var counter=10;
             id.innerHTML = "Time Out!!";
             window.location.href = "index-home.html";
         }
-    },1000);
+    },1000);*/
    
