a3ce4229866b3cbdebf48786b3f25b7e697e0e5c
Nathathaii <nathathaiaungudomsin@gmail.com>
2022-10-27 16:51:33 +0700

    pushhhh

--- script-my-account.mjs
+++ script-my-account.mjs
@@ -1,39 +1,43 @@
 // to use $ must include jQuery library in html file
 function displaytext(){
-    $.get('http://demo.api.booking.vtneil.space/api/users' ,function(data){
-        var userid = "001";
-        console.log(data[userid]);
-        document.getElementById("username").innerHTML = data[userid].name;
-        document.getElementById("studentid").innerHTML = data[userid].id;
-        document.getElementById("seatbutton").innerHTML = "F1-A01";//data[userid].current_seat_id;
-        document.getElementById("seattext").innerHTML = "My seat: " + data[userid].current_seat_id;
+    var userid = localStorage.getItem("current_id");
+    var url = 'http://demo.api.booking.vtneil.space/api/users/'+ userid;
+    $.get(url ,function(data){
+        document.getElementById("username").innerHTML = data.name;
+        document.getElementById("studentid").innerHTML = data.id;
+        document.getElementById("seatbutton").innerHTML = data.current_seat_id;
+        document.getElementById("seattext").innerHTML = "My seat: " + data.current_seat_id;
+        var whatsup = data.whatsup;
+        if(whatsup == ""){
+            document.getElementById("whatsup-btn").className = "want-normal-btn";
+            document.getElementById("whatsuptext").innerHTML = "no description";
+        }
+        else {
+            document.getElementById("whatsup-btn").className = whatsup;
+            var text = whatsup.replace("-", " ");
+            document.getElementById("whatsuptext").innerHTML = text;
+        } 
     });
 }
 displaytext();
 
-function updateUserData(newData){
-    //should delete old data before put 
-    // may wait for backend function 
-    request({ //add
-        url: 'http://demo.api.booking.vtneil.space/api/users',
-        type: "PUT",
-        json: newData,
-        contentType: "application/json; charset=utf-8",
+function disableTxt(){
+    var userid = localStorage.getItem("current_id");
+    var url = 'http://demo.api.booking.vtneil.space/api/users/'+ userid;
+    var newcap = document.getElementById("myCaption").innerHTML;
+    $.ajax({ 
+        url:  url,
+        type:"POST",
+        data: JSON.stringify({"caption": newcap}),
+        contentType: "application/json",
         dataType: "json",
-        // success: function (data, textStatus, jqXHR) {
-        //     console.log(data)
-        // }
+        success: function (textStatus) {
+            console.log(textStatus);
+        },
+        error: function (result, status) {
+            console.log(result);
+          }
     });
-}
-
-function disableTxt() { //save
-    document.getElementById("myCaption").disabled = true;
-    var newcaption = document.getElementById("savebt").value;
-    //wait for caption and set func from api
-    var newData = {
-        //all data with changed caption
-    }
-    updateUserData(newData);
 }
 
 function undisableTxt() {
